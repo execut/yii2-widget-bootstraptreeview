43398f324cbdf6b3abbb34a953c4b3b69cd68747
eXeCUT <sexecut@gmail.com>
2019-08-27 11:54:40 +0300

    Fixed bug with clear search input and expand

--- assets/TreeFilterInput.js
+++ assets/TreeFilterInput.js
@@ -19,13 +19,13 @@ $.widget("execut.TreeFilterInput", {
         });
 
         t.inputEl.keyup(function () {
-            // if (prevTimeout) {
-            //     clearTimeout(prevTimeout);
-            //     prevTimeout = false;
-            // }
+            if (prevTimeout) {
+                clearTimeout(prevTimeout);
+                prevTimeout = false;
+            }
 
-            // prevTimeout = setTimeout(function () {
-            //     prevTimeout = false;
+            prevTimeout = setTimeout(function () {
+                prevTimeout = false;
                 treeWidget.find('.list-group-item').css('display', '');
                 if (t.inputEl.val().length) {
                     treeWidget.treeview('collapseAll');
@@ -38,7 +38,6 @@ $.widget("execut.TreeFilterInput", {
                     findedNodes.map(function (node) {
                         $(node.$el).show();
                         t._expandAllParents([node]);
-                        // t._showAllSiblings([node]);
                     });
                     if (!findedNodes.length) {
                         t.inputEl.val(prevVal).keyup();
@@ -52,7 +51,7 @@ $.widget("execut.TreeFilterInput", {
                     var parentNode = treeWidget.treeview('getSelected');
                     t._expandAllParents(parentNode);
                 }
-            // }, t.delayBetweenSearch);
+            }, t.delayBetweenSearch);
         });
         t.inputEl.keypress(function () {
             if (isKillKeyPress) {
@@ -61,27 +60,23 @@ $.widget("execut.TreeFilterInput", {
             }
         });
     },
-    _showAllSiblings: function (nodes) {
-        var t = this,
-            treeWidget = t.treeWidget,
-            nodes = $(treeWidget.treeview('getSiblings', nodes));
-        nodes.each(function () {
-            this.$el.css('display', '');
-        });
-
-        t._showAllSiblings(nodes);
-    },
     _expandAllParents: function (parentNode) {
         var t = this,
             treeWidget = t.treeWidget;
         if (parentNode.length) {
             parentNode = parentNode[0];
             parentNode.$el.css('display', '');
+            var parents = [];
             while ((parentNode = treeWidget.treeview('getParents', parentNode)) && parentNode.length) {
-                parentNode = parentNode[0];
-                parentNode.$el.css('display', '');
-                treeWidget.treeview('expandNode', parentNode);
+                parentNode.map(function (node) {
+                    $(node.$el).css('display', '');
+                });
+                parents[parents.length] = parentNode;
             }
+
+            parents.reverse().map(function (parentNode) {
+                treeWidget.treeview('expandNode', parentNode);
+            });
         }
     }
 });
